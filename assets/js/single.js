e318df45fa725870aa99d3e5ee86be9b6e2e34cb
andrp <a.poklar@yahoo.com>
2021-03-18 17:46:38 -0500

    linked pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,21 @@
+var repoNameEl = document.querySelector("#repo-name");
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+}
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -20,7 +36,8 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+          document.location.replace("./index.html");
         }
       });
   };
@@ -76,4 +93,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-  getRepoIssues("facebook/react");
+getRepoName();
